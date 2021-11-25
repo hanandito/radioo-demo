9f99f05dbf73cede9e6973808ded06b2a0170bf4
hanandito <hananditos@gmail.com>
2021-11-25 22:28:59 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# digitalannualreport
+
+## Source API
+- https://bnpb-inacovid19.hub.arcgis.com/datasets/data-harian-kasus-per-provinsi-covid-19-indonesia/api
+- https://kawalcorona.com/api/
+- https://news.google.com/covid19/map?hl=en-ID&mid=%2Fm%2F03ryn&gl=ID&ceid=ID%3Aen&state=4
+- https://documenter.getpostman.com/view/16605343/Tzm6nwoS
+- https://api.covid19api.com/dayone/country/indonesia/status/confirmed
+- https://data.covid19.go.id/public/api/update.json
+- https://postman-toolboxes.github.io/covid-19/#featured-collections
+- https://docs.tradingeconomics.com/
+
+## Used
+- https://api.covid19api.com/dayone/country/indonesia/status/confirmed
+- https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json
+- https://covid19.mathdro.id/api/countries/ID/confirmed
+- https://api.tradingeconomics.com/historical/country/indonesia/indicator/gdp?c=guest:guest&format=json
---
+++ js/app.js
@@ -0,0 +1,59 @@
+// Page
+  $(document).ready(function() {
+    AOS.init({
+      duration: 1200
+    });
+    $('nav .burger').click(function() {
+      $('.sideNav').addClass("active");
+    })
+    $('.closeSideNav').click(function() {
+      $('.sideNav').removeClass("active");
+    })
+    $('.menu-icon').click(function() {
+      $('.side-menu').addClass("active");
+    })
+    $('.close-menu-icon').click(function() {
+      $('.side-menu').removeClass("active");
+    })
+  });
+
+  var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.image-header').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
+
+
+$(window).scroll(function(){
+  var scroll =  $(document).height() - $(window).height() - $(window).scrollTop();
+  $(".ninth-section").css({
+    backgroundSize:(100 + scroll/5) + "%"
+  })
+}
+)
